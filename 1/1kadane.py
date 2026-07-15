4b71d8d514d2c47269aff439ac76a675298b6fc5
= <=>
2026-07-15 11:18:45 +0530

    Kadane

---
+++ 1/1kadane.py
@@ -0,0 +1,31 @@
+# Kadane's algorithm :: Time O(N), Space O(1)
+# Find the maximum sub array
+
+num = [-2,1,-3,4,-1,2,1,-5,4]
+
+cur_sum = max_sum = float("-inf")
+for i in num:
+    cur_sum += i
+    if cur_sum < 0: cur_sum = 0
+    max_sum = max(cur_sum, max_sum)
+
+print(max_sum)
+
+#-----------------------------------
+# Brute Force O(n*n)
+
+num = [-2,1,-3,4,-1,2,1,-5,4]
+
+max_sum = 0
+for i in range(len(num)):
+    cur_sum = 0
+    for j in range(i, len(num)):
+        cur_sum = cur_sum + num[j]
+        max_sum = max(max_sum, cur_sum)
+
+print(max_sum)
+
+#--------------------------------------------
+# Sllinding window
+
+# ??
